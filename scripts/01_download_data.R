17833065002f009be23234aa0586f40cbb5a14d5
“jamiefaselt” <jamiefaselt@u.boisestate.edu>
2022-02-22 15:24:56 -0700

    added data for hmi from shared data folder

--- scripts/01_download_data.R
+++ scripts/01_download_data.R
@@ -89,3 +89,12 @@ gdrive_files <- drive_ls(folder)
 #have to treat the gdb as a folder and download it into a gdb directory in order to deal with the fact that gdb is multiple, linked files
 lapply(gdrive_files$id, function(x) drive_download(as_id(x),
                                                    path = paste0(here::here("data/original/LandMan_PrvtCons/"), gdrive_files[gdrive_files$id==x,]$name), overwrite = TRUE))
+
+
+# Download HMI for US Data ------------------------------------------------------
+folder_url <- "https://drive.google.com/drive/folders/1uBKCAeZ4VStT7N0HYy-yd0IbEyrtEdtE" # Metadata for Montana Private Conservation Lands
+folder <- drive_get(as_id(folder_url))
+gdrive_files <- drive_ls(folder)
+#have to treat the gdb as a folder and download it into a gdb directory in order to deal with the fact that gdb is multiple, linked files
+lapply(gdrive_files$id, function(x) drive_download(as_id(x),
+                                                   path = paste0(here::here("data/original/Human_Modification_Index/"), gdrive_files[gdrive_files$id==x,]$name), overwrite = TRUE))
