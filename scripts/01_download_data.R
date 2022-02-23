2e4c70c028f3e94d458dc095fa994f99a38c6a2b
jamiefaselt <jamiefaselt@u.boisestate.edu>
2022-02-23 10:56:25 -0700

    added zillow file

--- scripts/01_download_data.R
+++ scripts/01_download_data.R
@@ -98,3 +98,10 @@ gdrive_files <- drive_ls(folder)
 #have to treat the gdb as a folder and download it into a gdb directory in order to deal with the fact that gdb is multiple, linked files
 lapply(gdrive_files$id, function(x) drive_download(as_id(x),
                                                    path = paste0(here::here("data/original/Human_Modification_Index/"), gdrive_files[gdrive_files$id==x,]$name), overwrite = TRUE))
+# Download zillow land value Data ------------------------------------------------------
+folder_url <- "https://drive.google.com/drive/u/0/folders/18TDoj1-VTmcL-6nWbcmKtv7xXpVqO9Q3" # Metadata for Montana Private Conservation Lands
+folder <- drive_get(as_id(folder_url))
+gdrive_files <- drive_ls(folder)
+#have to treat the gdb as a folder and download it into a gdb directory in order to deal with the fact that gdb is multiple, linked files
+lapply(gdrive_files$id, function(x) drive_download(as_id(x),
+                                                   path = paste0(here::here("data/original/"), gdrive_files[gdrive_files$id==x,]$name), overwrite = TRUE))
