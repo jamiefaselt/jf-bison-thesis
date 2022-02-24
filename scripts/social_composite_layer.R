6a529765b64d142cbf3b886b9b54dc50b204afce
“jamiefaselt” <jamiefaselt@u.boisestate.edu>
2022-02-24 16:44:56 -0700

    updated social resistance with exp

--- scripts/social_composite_layer.R
+++ scripts/social_composite_layer.R
@@ -19,12 +19,14 @@ parceldensity <- raster("data/raster_layers/parcel_density_layer.tif")
 rc1.1m <- 1-bis.dec
 rc2.1m <- 1-cattle.sales
 rc3.1m <- 1-repub
-rc4.1m <- 1-landval.pnas
 rc5.1m <- 1-parceldensity
 
-fuz.sum <- 1-(rc1.1m*rc2.1m*rc3.1m*rc4.1m*rc5.1m)
+fuz.sum <- 1-(rc1.1m*rc2.1m*rc3.1m*rc5.1m)
 plot(fuz.sum) 
 
 writeRaster(fuz.sum, "data/raster_layers/social_composite_layer.tif", overwrite = TRUE)
-
+social_resistance <- (1+fuz.sum)^10 + landval.pnas/4
+plot(social_resistance)
+writeRaster(social_resistance, "data/raster_layers/social_resistance_layer.tif")
+social_comp <- raster("data/raster_layers/social_composite_layer.tif")
 
