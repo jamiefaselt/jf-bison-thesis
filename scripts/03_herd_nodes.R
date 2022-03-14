4968603d3f11bb29006d5207292e0c0c7814d491
“jamiefaselt” <jamiefaselt@u.boisestate.edu>
2022-03-14 13:33:54 -0600

    changed cmr to apr node

--- scripts/03_herd_nodes.R
+++ scripts/03_herd_nodes.R
@@ -11,50 +11,78 @@ library(tigris)
 library(dplyr)
 
 r <- raster("data/template_raster.tif")
+states <- tigris::states()
+mt <- states %>% filter(., NAME=="Montana", drop=TRUE)
+counties <- tigris::counties()
+mt.counties<-counties %>% filter(STATEFP %in%  c("30"))
+mt.counties<-st_transform(mt.counties,st_crs(r))
 
 #bring in the data, match projection and make valid 
 mt_reservations <- st_read("data/original/mt_reservations/MontanaReservations.shp") %>% 
   st_transform(.,st_crs(r)) %>% 
   st_make_valid()
 
-mt_fws <- st_read("data/original/mt_fws/MT_FWS.shp") %>% 
-  st_transform(.,st_crs(r))
-mt_CMR <- mt_fws %>% 
-  filter(., ORGNAME=="CHARLES M. RUSSELL NATIONAL WILDLIFE REFUGE",  drop=TRUE) %>% 
+#apr shapefiles from APR staff
+apr <- st_read("/Users/jamiefaselt/Downloads/AP_Property_Boundaries_011022/doc.kml") %>% 
   st_transform(.,st_crs(r)) %>% 
-  filter(., SUM_GISACR > 530517) %>% 
-  st_make_valid()
-plot(mt_CMR)
+  st_make_valid() 
+apr$area <- st_area(apr) %>% 
+  as.numeric(.)
 
+#trying to get this centroid to be in the middle of the three locations that have bison... but don't have the names on the dataframe
+lg.apr <- apr %>% 
+  filter(., area > 20000000)
+lg.apr<-lg.apr[!(lg.apr$Name=="73 Ranch"),]
+
+# OLD
+#mt_fws <- st_read("data/original/mt_fws/MT_FWS.shp") %>% 
+ # st_transform(.,st_crs(r))
+#mt_CMR <- mt_fws %>% 
+  #filter(., ORGNAME=="CHARLES M. RUSSELL NATIONAL WILDLIFE REFUGE",  drop=TRUE) %>% 
+  #st_transform(.,st_crs(r)) %>% 
+  #filter(., SUM_GISACR > 530517) %>% 
+  #st_make_valid()
+
+# Yellowstone
 mt_NPS <- st_read("data/original/nps_boundaries/NationalParkServiceAdminBoundaries_Montana.shp") %>% 
   st_transform(.,st_crs(r)) %>% 
   st_make_valid()
-
 yellowstone <- mt_NPS %>% 
   filter(., grepl('Yellowstone National Park',  UNIT_NAME))
 
+#subset and change names for mering
 rez <- subset(mt_reservations, select=c(geometry, NAME))
-cmr <- subset(mt_CMR, select=c(geometry, ORGNAME)) %>% 
-  rename(NAME = ORGNAME)
+lg.apr <- subset(lg.apr, select=c(geometry, Name)) %>%
+  rename(NAME = Name)
 yellowstone <- subset(yellowstone, select=c(geometry, UNIT_NAME)) %>%
   rename(NAME = UNIT_NAME)
-
-herds_shapefiles <- bind_rows(rez, cmr, yellowstone)
+herds_shapefiles <- bind_rows(rez, lg.apr, yellowstone)
 plot(st_geometry(herds_shapefiles))
-st_write(herds_shapefiles, "data/processed/herd_shapefile_outline.shp")
+st_write(herds_shapefiles, "data/processed/herd_shapefile_outline.shp", append = TRUE)
+
 # take the centroids
 rez.nodes <- st_centroid(rez)
-cmr.nodes <- st_centroid(cmr)
 nps.nodes <- st_centroid(yellowstone)
+# need to do some work to get this to have the right column names to combine them into one shapefile
+apr <- st_combine(lg.apr)
+apr.nodes <- st_centroid(apr) %>% 
+  st_as_sf %>% 
+  rename(geometry = x) 
+apr.nodes$NAME <-  seq(1, nrow(apr.nodes)) %>% 
+  as.character(.) %>% 
+  rename(apr.nodes$NAME==1)
+apr.nodes[apr.nodes$NAME==1, "NAME"] <- "American Prairie Reserve"
 
 #combine centroids into one shapefile
-all.nodes <- bind_rows(rez.nodes, cmr.nodes, nps.nodes)
+all.nodes <- bind_rows(rez.nodes, apr.nodes, nps.nodes)
 plot(all.nodes)
+plot(st_geometry(herds_shapefiles), add = TRUE)
 
 all.nodes$ID <- seq(1, nrow(all.nodes))
 all.nodes <- all.nodes %>% st_buffer(., 10000) # buffer of 10 km         
 plot(all.nodes)
-st_write(all.nodes, "data/processed/all_nodes_correct.shp", overwite = TRUE)
+st_write(all.nodes, "data/processed/all_nodes_correct.shp", append = TRUE)
 node.rast<-fasterize::fasterize(all.nodes, r, field = 'ID')
 plot(node.rast)
 writeRaster(node.rast, "data/processed/all_nodes.tif", overwrite = TRUE)
+
