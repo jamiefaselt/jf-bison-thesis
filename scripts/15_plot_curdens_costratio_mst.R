5f90dd1cc42d17411b82e3951c96a1d2ab3ad975
“jamiefaselt” <jamiefaselt@u.boisestate.edu>
2022-05-10 19:02:13 -0600

    not quite

--- scripts/15_plot_curdens_costratio_mst.R
+++ scripts/15_plot_curdens_costratio_mst.R
@@ -18,15 +18,19 @@ biophys.tr <- readRDS(here::here('data/Processed/TransitionLayers/biophystrans.r
 
 # Load Resistance surfaces ------------------------------------------------
 biophys.resist <- raster(here::here("data/raster_layers/biophys_resistance_layer.tif"))
-#biophys.resist[is.na(biophys.resist[])] <- 2* cellStats(biophys.resist, max)## drop NAs for costodistance
+biophys.resist[is.na(biophys.resist[])] <- 2* cellStats(biophys.resist, max)## drop NAs for costodistance
 implementation.resist1 <- raster(here::here("data/raster_layers/social_resistance_layer.tif"))
-#implementation.resist1[is.na(implementation.resist1[])] <- 5* cellStats(implementation.resist1, max)## drop NAs for costodistance
+implementation.resist1[is.na(implementation.resist1[])] <- 5* cellStats(implementation.resist1, max)## drop NAs for costodistance
 
 # Load centroids ----------------------------------------------------------
-pa.cents <- st_read("data/processed/herd_centroids.shp") %>% 
-  st_transform(. , crs(biophys.resist))  %>% 
+origins <- st_read(here::here("data/processed/all_nodes_correct.shp")) %>% 
   st_centroid(.) %>% 
   as(. , "Spatial")
+goals <- st_read(here::here("data/processed/all_nodes_correct.shp")) %>% 
+  st_centroid(.) %>% 
+  as(. , "Spatial")
+origin.proj <- spTransform(origins, crs(biophys.resist))
+goals.proj <- spTransform(goals, crs(biophys.resist))
 
 
 # Load k cost paths -------------------------------------------------------
@@ -37,7 +41,7 @@ biophys <- readRDS(here::here('Data/Processed/TransitionLayers/ms_tree.rds'))
 # Load biophys circuitscape run -------------------------------------------
 
 biophys.cs <- raster(here::here("data/circuitscape_outputs/biophys_resistance_layer/biophys_out_cum_curmap.asc"))
-#biophys.cs[is.na(biophys.cs[])] <- 5* cellStats(biophys.cs, max)## drop NAs for costodistance
+biophys.cs[is.na(biophys.cs[])] <- 5* cellStats(biophys.cs, max)## drop NAs for costodistance
 
 # Create Euclidean distance-----------------------------------------------------
 
@@ -45,10 +49,10 @@ euclidean.resist <- biophys.resist
 values(euclidean.resist) <- 1
 euclidean.tr <- transition(1/euclidean.resist, transitionFunction = mean, 16)
 euclidean.tr <- geoCorrection(euclidean.tr, "c")
-eucdist <- accCost(euclidean.tr, pa.cents)
+eucdist <- accCost(euclidean.tr, origin.proj)
 
 #extract values for last 50km
-euc.dist <- st_distance(as(pa.cents, "sf"), as(pa.cents, "sf"))
+euc.dist <- st_distance(as(origin.proj, "sf"), as(goals.proj, "sf"))
 units(euc.dist) <- NULL
 threshold <- as.vector(euc.dist - 10000)
 # Extract cost and current values -----------------------------------------
@@ -59,14 +63,14 @@ social1.lst <- social1[[1]]
 biophys.lst <- biophys[[1]]
 #all.lst <- c(social1.lst, social2.lst, biophys.lst)
 all.lst <- c(social1.lst, biophys.lst)
-soc.costdist1 <- accCost(social.tr1, pa.cents)
-bio.costdist <- accCost(biophys.tr, pa.cents)
+soc.costdist1 <- accCost(social.tr1, origin.proj)
+bio.costdist <- accCost(biophys.tr, origin.proj)
 #dist.stack <- stack(eucdist,bio.costdist, soc.costdist1, soc.costdist2)
-dist.stack <- stack(eucdist, bio.costdist, soc.costdist1, biophys.cs)
+dist.stack <- stack(eucdist,bio.costdist, soc.costdist1, biophys.cs)
 
 distance.extract <- lapply(1:length(all.lst), function(x) raster::extract(dist.stack, rasterToPolygons(all.lst[[x]], dissolve = TRUE)))
 #names(distance.extract) <- c("s1_1", "s1_2", "s1_3", "s1_4", "s1_5","s2_1", "s2_2", "s2_3", "s2_4", "s2_5","b1", "b2", "b3", "b4", "b5")
-names(distance.extract) <- c("s1", "s2", "s3","b1", "b2", "b3")
+names(distance.extract) <- c("s1", "s2", "s3", "b1", "b2", "b3")
 
 dist.df.list <- lapply(1:length(distance.extract), function(x) as.data.frame(distance.extract[[x]]))
 names(dist.df.list) <- names(distance.extract)
@@ -89,7 +93,7 @@ current.dens <- dist.cln %>%
   flextable::flextable() %>% 
   flextable::set_header_labels(., ID = "Cost Rank", max.dist = "Distance (km)", sum.current = "Total Current") 
 
-flextable::save_as_docx(current.dens, path = here::here("plots/table2_mst_draft2.docx"))
+flextable::save_as_docx(current.dens, path = here::here("plots/table2_BF_FP.docx"))
 
 # Estimate cost.ratio -----------------------------------------------------
 library(tidyr)
@@ -104,7 +108,7 @@ max.bphys.lcp <- dist.90 %>%
   dplyr::group_by(., LCPID) %>%
   dplyr::summarise(., max_biophys = max(bio.costdist, na.rm=TRUE))
 max.difs <- max.bphys.lcp
-max.difs[-1] <- lapply(max.bphys.lcp[-1], function(x) x - min(x, na.rm=TRUE))
+#max.difs[-1] <- lapply(max.bphys.lcp[-1], function(x) x - min(x, na.rm=TRUE))
 colnames(max.difs)[2] <- "delta_bio.costdist"
 
 dist.group <- dist.90 %>% 
@@ -131,10 +135,6 @@ p1 <- ggplot(data = filter(dist.sum, name == "abase"), mapping = aes(x = ID,
   guides(color = guide_colorbar(title = "\u0394 log(biophysical \ncost)")) +
   theme_bw()
 p1
-p2
-p3
-
-combined <- p1 + p2 + p3 + plot_layout(guides = 'collect') + plot_annotation(tag_levels = "A", tag_suffix = ")")
 
 
-ggsave('plots/fig5_BF_FP.png', p1)
+#ggsave('plots/fig5_BF_FP.png', p1)
