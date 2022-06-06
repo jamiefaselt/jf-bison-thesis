e73843f20e4fb809ddaf120c58f2a6d9f216f3e9
“jamiefaselt” <jamiefaselt@u.boisestate.edu>
2022-06-06 16:15:41 -0600

    fixed nas before resample

--- scripts/05_biophys_resistance_hsi_hmi.R
+++ scripts/05_biophys_resistance_hsi_hmi.R
@@ -24,17 +24,11 @@ r <- raster("data/template_raster.tif") %>%
 herds <- st_read("data/processed/herd_shapefile_outline.shp")
 # resample the hsi layer to match the extent and resolution of template raster
 hsi <- raster("data/original/SUMMER_HSI_clip/SUMMER_HSI_clip.tif")
-hsi # it says the max is 73 but the histogram shows that there are values over 100, Brock thinks this is from converting the file from ArcGIS
-# hist(hsi) this takes a while but shows how there are a bunch of values around 128
-hsi.resample <- resample(hsi, r, na.rm = TRUE) # choosing to resample here to get the extents and resolution match up-- already same projection since I built the template raster off of the hsi projection
-hsi.resample # something happens where the max is now 128
-plot(hsi.resample)
-#table(is.na(hsi.resample[]))
-# according to Brent (creater of hsi layer) the max value should be 73 NOT 128
-# fix the max value here
-hsi.resample[hsi.resample>73] <- NA
-hsi.resample 
-plot(hsi.resample, colNA="red")
+hsi.crop <- crop(hsi, r)
+hsi.crop[hsi.crop>73] <- NA # it says the max is 73 but the histogram shows that there are values over 100, Brock thinks this is from converting the file from ArcGIS
+
+hsi.resample <- terra::resample(hsi.crop, r, na.rm = TRUE) 
+
 #write this for future use so I won't have to resample again!
 writeRaster(hsi.resample, "data/processed/hsi_resample.tif", overwrite = TRUE) 
 
@@ -53,7 +47,7 @@ hsi.rescale
 
 # bring in the human modification layer
 hmi <- raster("data/original/Human_Modification_Index/prj.adf")
-hmi.resample <- resample(hmi,r)
+hmi.resample <- terra::resample(hmi,r)
 plot(hmi.resample)
 ext(hmi.resample)
 ext(r)
@@ -68,7 +62,7 @@ fuzzysum <- function(r1, r2) {
   rc2.1m <- (1-r2)
   fuz.sum <- 1-(rc1.1m*rc2.1m)
 }
-biophys_fuzsum <- fuzzysum(hsi.rescale, hmi)
+biophys_fuzsum <- fuzzysum(hsi.rescale, hmi.resample)
 plot(biophys_fuzsum, col=plasma(256), axes = TRUE, main = "HSI+HMI Resistance Layer")
 
 # make into resistance surface
@@ -78,7 +72,6 @@ plot(biophys_mask, col=plasma(256), axes = TRUE, main = "HSI+HMI Resistance Laye
 
 #write raster (saving both gdrive and local computer)
 writeRaster(biophys_mask, "data/raster_layers/biophys_resistance_layer.tif", overwrite = TRUE)
-
 
 # now we have the biophysical resistance surface based on the habitat suitability index and human modification layers combined with a fuzzy sum approach and turned into a resistance layer using methods from Dickson et al.
 
