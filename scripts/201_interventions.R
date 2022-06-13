a57daf653a66caf251bcf20d8c8c91a17c708b65
“jamiefaselt” <jamiefaselt@u.boisestate.edu>
2022-06-13 05:43:53 -0600

    updated to two shortcircuit regions

--- scripts/201_interventions.R
+++ scripts/201_interventions.R
@@ -96,31 +96,66 @@ extent(tribal.scenario)==extent(biophys.resist)
 
 # DOI Friendly Scenario ---------------------------------------------------
 
-# template raster
+# bring in hsi and temp raster
 r <- raster("data/template_raster.tif")
+#bring in counties
+counties <- tigris::counties(state=c("WY", "MT"))
+# make columns match to caddat
+counties <- rename(counties, State.ANSI = STATEFP)
+counties <- rename(counties, County.ANSI = COUNTYFP)
+#counties$State.ANSI <- as.numeric(counties$State.ANSI)#don't do this; these should stay characters, it may not cause errors here but it will if there are leading 0's
+#counties$County.ANSI <- as.numeric(counties$County.ANSI)
+#wy.counties<-counties %>% filter(State.ANSI %in%  c("56"))
+cty<-st_transform(counties,st_crs(r))
+cty$cty.area <- st_area(cty)
+cty$cty.area <- as.numeric(cty$cty.area) 
 
-# herd locations
-herds <- st_read("data/processed/herd_shapefile_outline.shp")
 
-#load the PADUS
-mt.padus <- st_read("data/original/PADUS2_1_StateMT_Shapefile/PADUS2_1Designation_StateMT.shp") 
-#plot(st_geometry(mt.padus))
-st_make_valid(mt.padus)
-st_is_valid(mt.padus)
-head(mt.padus)
+##### PA Area #####
+# Function to fix PADUS geometries ----------------------------------------
 
-# reproject
-reproj <- st_transform(mt.padus, crs = st_crs(r)) %>% 
+# need to bring both of these datasets in 
+wy.desig <- st_read("data/original/PADUS2_1_StateWY_Shapefile/PADUS2_1Designation_StateWY.shp") %>% 
+  st_make_valid() 
+wy.proc <- st_read("data/original/PADUS2_1_StateWY_Shapefile/PADUS2_1Proclamation_StateWY.shp") %>% 
+  st_make_valid() 
+mt.desig <- st_read("data/original/PADUS2_1_StateMT_Shapefile/PADUS2_1Designation_StateMT.shp") %>% 
+  st_make_valid()
+mt.proc <- st_read("data/original/PADUS2_1_StateMT_Shapefile/PADUS2_1Proclamation_StateMT.shp") %>% 
+  st_make_valid()
+
+#drop proclamation polys that can contain parcelable land
+procs <- bind_rows(wy.proc, mt.proc) %>% 
+  filter(., Des_Tp != "TRIBL" | is.na(Des_Tp)) %>% 
+  filter(., Loc_Ds != "FSA" | is.na(Loc_Ds)) %>% 
+  filter(., Loc_Ds != "WMD" | is.na(Loc_Ds)) %>% 
+  st_transform(., st_crs(r))
+
+# no polys in des that could include parcealable land so not dropping any
+des <- bind_rows(wy.desig, mt.desig) %>% 
+  st_transform(., st_crs(r))
+
+
+# join them
+join <- bind_rows(des, procs)
+
+# find out where they don't overlap with one of the pa datasets
+diffPoly <- st_difference(join, st_union(procs))
+
+plot(st_geometry(diffPoly))
+
+# join that with the padus dataset -- should now have both pas with no overlap
+join.new <- bind_rows(diffPoly, procs) %>% 
   st_make_valid(.)
-#plot(st_geometry(reproj))
-head(reproj)
 
 # filter and buffer
-large.pas <- reproj %>% 
+large.pas <- join.new %>% 
   filter(., GIS_Acres > 50000) 
 head(large.pas)
 
 large.pas$ID <- seq(1, nrow(large.pas))
+
+large.pas <- st_crop(large.pas, r)
 
 
 #bring in habitat suitability and social composite resistance layers
@@ -141,21 +176,22 @@ join <- left_join(large.pas, bio.extract) %>%
 head(join)
 df <- subset(join, select = c(Unit_Nm, ID, social_resistance_layer, hsi_resample,  d_Des_Tp, geometry))
 
+
 # write in case you want to change the selection from the same extract 
-st_write(df, "data/processed/pa_bio_social_extract.shp")
+st_write(df, "data/processed/pa_bio_social_extract.shp", append = FALSE)
 
 #find the upper 75% of habitat suitability
 hab.qual<-quantile(df$hsi_resample,probs=c(0.25, 0.5,0.75, .9))
 hab.qual
 
 hab.75 <- df %>% 
-  filter(., hsi_resample > 39.68)
+  filter(., hsi_resample > 30.86)
 
 #find the lowest 50% of social composite
 resistance<-quantile(hab.75$social_resistance_layer,probs=c(0,0.25,0.5,0.75, .9))
 resistance
 composite <- hab.75 %>% 
-  filter(., social_resistance_layer < 629.5 ) %>% 
+  filter(., social_resistance_layer < 529 ) %>% 
   st_transform(., st_crs(r))
 
 
@@ -168,7 +204,11 @@ st_write(composite, "data/processed/publand_shortcircuit.shp", append = FALSE)
 new.herds <- st_read("data/processed/publand_shortcircuit.shp")
 new.herds$ID <- seq(1, nrow(new.herds))
 
-new.herds <- new.herds %>% 
+shortcircuit <- new.herds[new.herds$Unit_Nm %in% c("Grand Teton National Park", "Rocky Mountain Front Conservation Area"),] 
+
+
+
+new.herds <- shortcircuit %>% 
   group_by(ID) %>%
   summarise(geometry = sf::st_union(geometry)) 
 
@@ -176,6 +216,10 @@ new.herds <- new.herds %>%
 newherd.rast<-fasterize::fasterize(new.herds, r, field = 'ID')
 plot(newherd.rast)
 writeRaster(newherd.rast, "data/processed/shortcircuit.tif", overwrite= TRUE)
+
+herds <- st_read("data/processed/herd_shapefile_outline.shp")
+plot(st_geometry(herds), add = TRUE)
+
 
 # the same composite fuzzy sum approach is used for a resistance layer but the shortcircuit file is added to the ini file for CS
 
